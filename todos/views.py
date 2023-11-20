b23c9a02473dd2b7f3d6995ea6c9d91fbd938683
bruan05 <bmiguelandrade.bm@gmail.com>
2023-11-20 15:29:39 -0300

    alpha project.
    Terminando a versão alfa do projeto, foram realizadas algumas mudanças e adições no código.

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 
-# Create your views here.
+from .models import Todo
 
-
-def home(request):
-    # return HttpResponse("Seja bem vindo ao projeto!!!")
-    return render(request, "todos/home.html")
+class TodoListView(ListView):
+    model = Todo
+    
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+    
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+    
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+    
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")
+    
