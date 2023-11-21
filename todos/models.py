168c4804d23fd21ddf5feb0186ba707cd39dd145
bruan05 <bmiguelandrade.bm@gmail.com>
2023-11-21 12:56:45 -0300

    1.3

--- todos/models.py
+++ todos/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(verbose_name="Nome do Pet", max_length=100, null=False, blank=False)
-    breed = models.CharField(verbose_name="Tipo de Pet", max_length=100, null=False, blank=False)
+    breed = models.CharField(verbose_name="Raça do Pet", max_length=100, null=True, blank=False)
+    tipe = models.CharField(verbose_name="Tipo de Pet", max_length=100, null=True, blank=False)
+    color = models.CharField(verbose_name="Cor do Pet", max_length=100, null=True, blank=False)
+    genero = models.CharField(verbose_name="Genero", max_length=100, null=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField(verbose_name="Data de Retirada", null=False, blank=False)
     finished_at = models.DateField(null=True)
--- todos/views.py
+++ todos/views.py
@@ -9,12 +9,12 @@ class TodoListView(ListView):
     
 class TodoCreateView(CreateView):
     model = Todo
-    fields = ["title", "deadline"]
+    fields = ["title","tipe","breed","color","genero", "deadline"]
     success_url = reverse_lazy("todo_list")
     
 class TodoUpdateView(UpdateView):
     model = Todo
-    fields = ["title", "deadline"]
+    fields = ["title","tipe","breed","color","genero", "deadline"]
     success_url = reverse_lazy("todo_list")
     
 class TodoDeleteView(DeleteView):
